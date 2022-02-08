687bc000f7cd23373917f286f9af34fce3ce2c90
ShahnozaSh <71026485+ShahnozaSh@users.noreply.github.com>
2022-02-08 13:01:56 +0600

    Initial Commit

---
+++ ToDoListApp/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  ToDoListApp
+//
+//  Created by Шахноза on 8/2/22.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var description: String
+    var completed: Bool = false
+}
+
+
